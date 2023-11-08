272862a3c091d69e2177c10aa64a3817e2becec7
shakyacs <usace.cn@gmail.com>
2023-11-08 13:47:09 +0800

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -1,4 +1,6 @@
 import type { AppInfo } from '@/types/app'
+export const APP_ID = '09b55bb2-58c3-4458-88f0-58a24b79983b'
+export const API_KEY = '<app-7xNk1isiF5jJNNklyp26RSzi>'
 export const APP_ID = `${process.env.NEXT_PUBLIC_APP_ID}`
 export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
 export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
@@ -18,5 +20,4 @@ export const API_PREFIX = '/api'
 export const LOCALE_COOKIE_NAME = 'locale'
 
 export const DEFAULT_VALUE_MAX_LEN = 48
-export const APP_ID = '09b55bb2-58c3-4458-88f0-58a24b79983b'
-export const API_KEY = '<app-7xNk1isiF5jJNNklyp26RSzi>'
+
